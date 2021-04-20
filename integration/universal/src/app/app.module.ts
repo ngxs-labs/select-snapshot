047b9aa6cd54b5be2ebe297eba9d84d1cc90b7c3
arturovt <arthurandrosovich@gmail.com>
2021-04-20 21:43:30 +0300

    chore: update integration example with asynchronously loaded component

--- integration/universal/src/app/app.module.ts
+++ integration/universal/src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsSelectSnapshotModule } from '@ngxs-labs/select-snapshot';
 
 import { ProgressState } from './store';
 
 import { AppComponent } from './app.component';
-import { ProgressComponent } from './components/progress/progress.component';
 
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'universal-select-snapshot' }),
-    HttpClientModule,
     NgxsModule.forRoot([ProgressState]),
     NgxsSelectSnapshotModule.forRoot(),
   ],
-  declarations: [AppComponent, ProgressComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
