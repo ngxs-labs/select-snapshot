b48cefb6ff988e49a78800cdd35cafae4bc66400
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2023-04-08 22:02:44 +0300

    Revert "Merge branch 'master' of https://github.com/vanmxpx/select-snapshot"
    This reverts commit 42f64c213c064b152e430d84096bfb515326546c, reversing
    changes made to 3fb59fc4462a082fbedc6dcba08c46eeefc63c65.

---
+++ integration/universal/src/app/progress/progress.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { ProgressComponent } from './progress.component';
+
+@NgModule({
+  declarations: [ProgressComponent],
+})
+export class ProgressModule {}
+
+export { ProgressComponent } from './progress.component';
